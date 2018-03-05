d2b7ebd3909b481c0086b874c44eef2e47e3466b
wanglizhong0118 <wanglizhong0118@gmail.com>
2018-03-05 11:00:25 +0100

    Feat: add test cases for class helpFunc

--- ci-server/src/test/test_compileRepository.java
+++ ci-server/src/test/test_compileRepository.java
@@ -1,14 +1,7 @@
 package test;
 
 import java.io.File;
-import java.io.IOException;
-import java.security.acl.AclNotFoundException;
-
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
-
-import app.compileRepository;
 
 class test_compileRepository {
 
@@ -18,14 +11,9 @@ class test_compileRepository {
     String logFilePath_exists = "C://Users/allwi/Documents/GitHub/ci-server-temp-unittest/test_log.txt";
     String logFilePath_not_exists = "C://Users/allwi/Documents/GitHub/ci-server-temp-unittest-fake/test_log.txt";
 
-    @Rule
-    public ExpectedException fileExceptin = ExpectedException.none();
-    public ExpectedException logExceptin = ExpectedException.none();
-
     @Test
     public void test_compile() {
-        fileExceptin.expect(AclNotFoundException.class);
-        compileRepository.init(localtmpPath_not_exists, logFilePath_not_exists);
+
     }
 
 }
